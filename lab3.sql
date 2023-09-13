581f86b067298ff006451a067cf4279669c2190e
hayden-daly <143658577+hayden-daly@users.noreply.github.com>
2023-09-13 18:04:01 -0400

    Update lab3.sql

--- lab3.sql
+++ lab3.sql
@@ -30,3 +30,11 @@ values ('p3@jmu.edu', 'p3', 'o3') ;
 
 insert into course (c_number, c_name, room, p_email)
 values ('c4', 'facebook','r1', 'p3@jmu.edu')
+
+update course 
+set p_email = 'p3@jmu.edu'
+where p_email = 'p1@jmu.edu';
+
+delete from professor 
+where p_email = 'p1@jmu.edu'
+
